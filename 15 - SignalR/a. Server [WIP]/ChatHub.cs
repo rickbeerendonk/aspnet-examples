001c167612685856efd511a60e403a67425cba42
Rick Beerendonk <rick@beerendonk.com>
2024-01-31 01:51:10 +0100

    Add WIP.

---
+++ 15 - SignalR/a. Server [WIP]/ChatHub.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace Server.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
