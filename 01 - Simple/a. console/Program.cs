e332652f30081ebe4d62eea3ca3fd1c3b4d13392
Rick Beerendonk <rick@beerendonk.com>
2025-02-10 23:05:31 +0100

    Standard .NET 9 setup.

--- 01 - Simple/a. console/Program.cs
+++ 01 - Simple/a. console/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// European Union Public License version 1.2
+// Copyright © 2021 Rick Beerendonk
+
+Console.WriteLine($"Hello, .NET {Environment.Version}!");
