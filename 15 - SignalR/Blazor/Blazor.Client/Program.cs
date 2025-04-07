222df98f8877e050dcc778f1a4b285dc2b44e708
Rick Beerendonk <rick@beerendonk.com>
2025-04-07 12:30:31 +0200

    Add SignalR Chat example.

--- 15 - SignalR/Blazor/Blazor.Client/Program.cs
+++
@@ -1,8 +0,0 @@
-// European Union Public License version 1.2
-// Copyright © 2024 Rick Beerendonk
-
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-await builder.Build().RunAsync();
